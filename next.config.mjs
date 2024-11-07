bd570a958634a263e79c06dc2ad5f6fb3b2d5cd4
Muhammed Minhaj Mahroof <72432356+minhaaj-t@users.noreply.github.com>
2024-11-08 01:04:40 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = {
+  images: {
+    domains: ['https://i.postimg.cc'], // Add the domain of the external images
+  },
+};
 
 export default nextConfig;
